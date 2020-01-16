d1abfa6beb800005287feec0e5285c1c2b5e079b
xiaoweixiao <1679204951@qq.com>
2020-01-16 17:46:05 +0800

    first push

---
+++ five_circle.py
@@ -0,0 +1,37 @@
+import turtle
+t = turtle.Pen()
+
+t.width(8)
+
+t.color('blue')
+t.circle(50)
+
+t.penup()
+t.forward(130)
+t.pendown()
+
+t.color('black')
+t.circle(50)
+
+t.penup()
+t.forward(130)
+t.pendown()
+
+t.color('red')
+t.circle(50)
+
+t.penup()
+t.goto(66,-50)
+t.pendown()
+
+t.color('yellow')
+t.circle(50)
+
+t.penup()
+t.forward(130)
+t.pendown()
+
+t.color('green')
+t.circle(50)
+
+
